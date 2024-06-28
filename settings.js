045052dfc1eb4b1b5fc2f2189e766758929eece4
SmokeAKV <az998057959@gmail.com>
2024-06-27 22:43:00 -0300

    primeiro commit

---
+++ settings.js
@@ -0,0 +1,24 @@
+{
+Papai = 'Smoke Akv';
+BotName = 'Ceci V1.5';
+numeroBot = '557499710769';
+ctt = 'wa.me/557499126457';
+nmrdono = '557499126457';
+nmrdonoo = '5511953641743';
+nmrdn = '+55 (74) 99912-6457';
+linktrr = 'linktr.ee/makima.bot';
+groupName = 'rapaz';
+instag = 'Shelly Akv';
+ChatNaka = 'https://chat.whatsapp.com/GDz6olREQcQCXoBnreA8M2';
+prefix = ['!']
+prefixobot = ['!']
+global.prefa = ['','!','.']
+
+
+global.author = 'smoke' //Preencha aqui como o primeiro campo Para o seu NOME ( NOME DA FIGURINHA )
+
+global.packname = 'Bkt' // Preencha aqui como o primeiro campo visivel a marca d'agua dos stickers. ( NOME DA FIGURINHA )
+
+global.packname2 = 'Smokiiin' // Preencha aqui como o segundo campo visivel a marca d'agua dos stickers.
+
+}
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+clear
+while :
+do
+echo "\033[1;35m SÓ UM MOMENTO \033[0m" " \033[1;33m INCIANDO CECÍLIA BOT V1.0 \033[0m  "
+    node main.js
+    sleep 1
+
+done
